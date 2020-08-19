051381fdb60be65fb46c142c6ec69a10002ee5c2
anacsv <anapaula.carneiro@olist.com>
2020-08-19 17:04:40 -0300

    Add start class

---
+++ app/start.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api
+from flask_cors import CORS
+
+from app.controller.selller_order_controller
+
+app = Flask(__name__)
+api = Api(app)
+cord = CORS(app)
+
+@app.route('/')
+def initial():
+    return ''
+
+api.add_resource(
+    '/api/sellers_order/', '/api/sellers_order/<int:id>', endpoint = 'sellers_order'
+)
+
+app.run(debug=True)
