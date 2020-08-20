912bd8aa52632f3488756b7cef8b6268cac728de
anacsv <anapaula.carneiro@olist.com>
2020-08-20 10:50:19 -0300

    Adjusted seller order model

--- app/model/selller_order_model.py
+++ app/model/selller_order_model.py
@@ -138,323 +138,323 @@ class SellerOrderModel():
     # --------------------------- code
     @property
     def code(self) -> str:
-        return self.__code
+        return str(self.__code)
 
     @code.setter
     def code(self, code: str):
-        self.__code = code
+        self.__code = str(code)
 
     # --------------------------- order_id
     @property
     def order_id(self) -> str:
-        return self.__order_id
+        return str(self.__order_id)
 
     @order_id.setter
     def order_id(self, order_id: str):
-         self.__order_id = order_id
+         self.__order_id = str(order_id)
 
     # --------------------------- channel_slug
     @property
     def channel_slug(self) -> str:
-        return self.__channel_slug
+        return str(self.__channel_slug)
 
     @channel_slug.setter
     def channel_slug(self, channel_slug: str):
-        self.__channel_slug = channel_slug
+        self.__channel_slug = str(channel_slug)
 
      # --------------------------- channel_store
     @property
     def channel_store(self) -> str:
-        return self.__channel_store
+        return str(self.__channel_store)
 
     @channel_store.setter
     def channel_store(self, channel_store: str):
-        self.__channel_store = channel_store
+        self.__channel_store = str(channel_store)
 
     # ------------------------------ seller_id
     @property
     def seller_id(self) -> str:
-        return self.__seller_id
+        return str(self.__seller_id)
 
     @seller_id.setter
     def seller_id(self, seller_id: str):
-        self.__seller_id = seller_id
+        self.__seller_id = str(seller_id)
 
     # ------------------------------- seller_name
     @property
     def seller_name(self) -> str:
-        return self.__seller_name
+        return str(self.__seller_name)
 
     @seller_name.setter
     def seller_name(self, seller_name: str):
-        self.__seller_name = seller_name
+        self.__seller_name = str(seller_name)
 
     # -------------------------------- purchase_timestamp
     @property
     def purchase_timestamp(self) -> str:
-        return self.__purchase_timestamp
+        return str(self.__purchase_timestamp)
 
     @purchase_timestamp.setter
     def purchase_timestamp(self, purchase_timestamp: str):
-        self.__purchase_timestamp = purchase_timestamp
+        self.__purchase_timestamp = str(purchase_timestamp)
 
     # ---------------------------------- status
     @property
     def status(self) -> str:
-        return self.__status
+        return str(self.__status)
 
     @status.setter
     def status(self, status: str):
-        self.__status = status
+        self.__status = str(status)
 
     # ---------------------------------- approved_at
     @property
     def approved_at(self) -> str:
-        return self.__approved_at
+        return str(self.__approved_at)
 
     @approved_at.setter
     def approved_at(self, approved_at: str):
-        self.__approved_at = approved_at
+        self.__approved_at = str(approved_at)
 
     # ---------------------------------- shipping_limit_date
     @property
     def shipping_limit_date(self) -> str:
-        return self.__shipping_limit_date
+        return str(self.__shipping_limit_date)
 
     @shipping_limit_date.setter
     def shipping_limit_date(self, shipping_limit_date: str):
-        self.__shipping_limit_date
+        self.__shipping_limit_date = str(shipping_limit_date)
 
     # ---------------------------------- availability_days
     @property
     def availability_days(self) -> str:
-        return self.__availability_days
+        return str(self.__availability_days)
 
     @availability_days.setter
     def availability_days(self, availability_days: str):
-        self.__availability_days = availability_days
+        self.__availability_days = str(availability_days)
 
     # ---------------------------------- invoice_url
     @property
     def invoice_url(self) -> str:
-        return self.__invoice_url
+        return str(self.__invoice_url)
 
     @invoice_url.setter
     def invoice_url(self, invoice_url: str):
-        self.__invoice_url = invoice_url
+        self.__invoice_url = str(invoice_url)
 
     # ---------------------------------- invoice_issue_date
     @property
     def invoice_issue_date(self) -> str:
-        return self.__invoice_issue_date
+        return str(self.__invoice_issue_date)
 
     @invoice_issue_date.setter
     def invoice_issue_date(self, invoice_issue_date: str):
-        self.__invoice_issue_date = invoice_issue_date
+        self.__invoice_issue_date = str(invoice_issue_date)
 
     # ---------------------------------- invoice_key
     @property
     def invoice_key(self) -> str:
-        return self.__invoice_key
+        return str(self.__invoice_key)
 
     @invoice_key.setter
     def invoice_key(self, invoice_key: str):
-        self.__invoice_key = invoice_key
+        self.__invoice_key = str(invoice_key)
 
     # ---------------------------------- invoice_number
     @property
     def invoice_number(self) -> int:
-        return self.invoice_number
+        return str(self.invoice_number)
 
     @invoice_number.setter
     def invoice_number(self, invoice_number: int):
-        self.__invoice_number = invoice_number
+        self.__invoice_number = str(invoice_number)
 
     # ---------------------------------- invoice_serial_number
     @property
     def invoice_serial_number(self) -> int:
-        return self.__invoice_serial_number
+        return str(self.__invoice_serial_number)
 
     @invoice_serial_number.setter
     def invoice_serial_number(self, invoice_serial_number: int):
-        self.__invoice_serial_number = invoice_serial_number
+        self.__invoice_serial_number = str(invoice_serial_number)
 
     # ------------------------------------ customer_id
     @property
     def customer_id(self) -> str:
-        return self.__customer_id
+        return str(self.__customer_id)
 
     @customer_id.setter
     def customer_id(self, customer_id: str):
-        self.__customer_id = customer_id
+        self.__customer_id = str(customer_id)
 
     # ------------------------------------ shipment_id
     @property
     def shipment_id(self) -> str:
-        return self.__shipment_id
+        return str(self.__shipment_id)
 
     @shipment_id.setter
     def shipment_id(self, shipment_id: str):
-        self.__shipment_id = shipment_id
+        self.__shipment_id = str(shipment_id)
 
     # ------------------------------------ invoice_source
     @property
     def invoice_source(self) -> str:
-        return self.__invoice_source
+        return str(self.__invoice_source)
 
     @invoice_source.setter
     def invoice_source(self, invoice_source: str):
-        self.__invoice_source = invoice_source
+        self.__invoice_source = str(invoice_source)
 
     # ------------------------------------ delivered_customer_date
     @property
     def delivered_customer_date(self) -> str:
-        return self.__delivered_customer_date
+        return str(self.__delivered_customer_date)
 
     @delivered_customer_date.setter
     def delivered_customer_date(self, delivered_customer_date: str):
-        self.__delivered_customer_date = delivered_customer_date
+        self.__delivered_customer_date = str(delivered_customer_date)
 
     # ------------------------------------ seller_brand
     @property
     def seller_brand(self) -> str:
-        return self.__seller_brand
+        return str(self.__seller_brand)
 
     @seller_brand.setter
     def seller_brand(self, seller_brand: str):
-        self.__seller_brand = seller_brand
+        self.__seller_brand = str(seller_brand)
 
     # ------------------------------------ seller_email
     @property
     def seller_email(self) -> str:
-        return self.__seller_email
+        return str(self.__seller_email)
 
     @seller_email.setter
     def seller_email(self, seller_email: str):
-        self.__seller_email = seller_email
+        self.__seller_email = str(seller_email)
 
     # ------------------------------------ invoice_danfe_url
     @property
     def invoice_danfe_url(self) -> str:
-        return self.__invoice_danfe_url
+        return str(self.__invoice_danfe_url)
 
     @invoice_danfe_url.setter
     def invoice_danfe_url(self, invoice_danfe_url: str):
-        self.__invoice_danfe_url = invoice_danfe_url
+        self.__invoice_danfe_url = str(invoice_danfe_url)
 
     # ------------------------------------ cancelation_reason
     @property
     def cancelation_reason(self) -> str:
-        return self.__cancelation_reason
+        return str(self.__cancelation_reason)
 
     @cancelation_reason.setter
     def cancelation_reason(self, cancelation_reason: str):
-        self.__cancelation_reason = cancelation_reason
+        self.__cancelation_reason = str(cancelation_reason)
 
     # ------------------------------------ cancelation_status
     @property
     def cancelation_status(self) -> str:
-        return self.__cancelation_status
+        return str(self.__cancelation_status)
 
     @cancelation_status.setter
     def cancelation_status(self, cancelation_status: str):
-        self.__cancelation_status = cancelation_status
+        self.__cancelation_status = str(cancelation_status)
 
     # ------------------------------------ suspension_reason
     @property
     def suspension_reason(self) -> str:
-        return self.__suspension_reason
+        return str(self.__suspension_reason)
 
     @suspension_reason.setter
     def suspension_reason(self, suspension_reason: str):
-        self.__suspension_reason = suspension_reason
+        self.__suspension_reason = str(suspension_reason)
 
     # ------------------------------------ estimated_delivery_date
     @property
     def estimated_delivery_date(self) -> str:
-        return self.__estimated_delivery_date
+        return str(self.__estimated_delivery_date)
 
     @estimated_delivery_date.setter
     def estimated_delivery_date(self, estimated_delivery_date: str):
-        self.__estimated_delivery_date = estimated_delivery_date
+        self.__estimated_delivery_date = str(estimated_delivery_date)
 
     # ------------------------------------ invoice_status
     @property
     def invoice_status(self) -> str:
-        return self.__invoice_status
+        return str(self.__invoice_status)
 
     @invoice_status.setter
     def invoice_status(self, invoice_status: str):
-        self.__invoice_status = invoice_status
+        self.__invoice_status = str(invoice_status)
 
     # ------------------------------------ invoice_id
     @property
     def invoice_id(self) -> str:
-        return self.__invoice_id
+        return str(self.__invoice_id)
 
     @invoice_id.setter
     def invoice_id(self, invoice_id: str):
-        self.__invoice_id = invoice_id
+        self.__invoice_id = str(invoice_id)
 
     # ------------------------------------ branded_store_slug
     @property
     def branded_store_slug(self) -> str:
-        return self.__branded_store_slug
+        return str(self.__branded_store_slug)
 
     @branded_store_slug.setter
     def branded_store_slug(self, branded_store_slug: str):
-        self.__branded_store_slug = branded_store_slug
+        self.__branded_store_slug = str(branded_store_slug)
 
     # ------------------------------------ search_vector
     @property
     def search_vector(self) -> str:
-        return self.__search_vector
+        return str(self.__search_vector)
 
     @search_vector.setter
     def search_vector(self, search_vector: str):
-        self.__search_vector = search_vector
+        self.__search_vector = str(search_vector)
 
     # ------------------------------------ shipping_quote_group_id
     @property
     def shipping_quote_group_id(self) -> str:
-        return self.__shipping_quote_group_id
+        return str(self.__shipping_quote_group_id)
 
     @shipping_quote_group_id.setter
     def shipping_quote_group_id(self, shipping_quote_group_id: str):
-        self.__shipping_quote_group_id = shipping_quote_group_id
+        self.__shipping_quote_group_id = str(shipping_quote_group_id)
 
     # ------------------------------------ payer_id
     @property
     def payer_id(self) -> str:
-        return self.__payer_id
+        return str(self.__payer_id)
 
     @payer_id.setter
     def payer_id(self, payer_id: str):
-        self.__payer_id = payer_id
+        self.__payer_id = str(payer_id)
 
     # ------------------------------------ display_status
     @property
     def display_status(self) -> str:
-        return self.__display_status
+        return str(self.__display_status)
 
     @display_status.setter
     def display_status(self, display_status: str):
-        self.__display_status = display_status
+        self.__display_status = str(display_status)
 
     # ------------------------------------ estimated_delivery_shift
     @property
     def estimated_delivery_shift(self) -> str:
-        return self.__estimated_delivery_shift
+        return str(self.__estimated_delivery_shift)
 
     @estimated_delivery_shift.setter
     def estimated_delivery_shift(self, estimated_delivery_shift: str):
-        self.__estimated_delivery_shift = estimated_delivery_shift
+        self.__estimated_delivery_shift = str(estimated_delivery_shift)
 
     # ------------------------------------ invoice_error
     @property
     def invoice_error(self) -> str:
-        return self.__invoice_error
+        return str(self.__invoice_error)
 
     @invoice_error.setter
     def invoice_error(self, invoice_error: str):
-        self.__invoice_error = invoice_error
+        self.__invoice_error = str(invoice_error)
--- app/start.py
+++ app/start.py
@@ -2,6 +2,25 @@ from flask import Flask, jsonify, request
 from flask_restful import Api
 from flask_cors import CORS
 
+from app.controller.selller_order_controller import SellerOrderController
+
+app = Flask(__name__)
+api = Api(app)
+cord = CORS(app)
+
+@app.route('/')
+def initial():
+    return ''
+
+api.add_resource(
+    SellerOrderController, '/api/sellers_order/', '/api/sellers_order/<id>', endpoint = 'sellers_order'
+)
+
+app.run(debug=True)
+from flask import Flask, jsonify, request
+from flask_restful import Api
+from flask_cors import CORS
+
 from app.controller.selller_order_controller
 
 app = Flask(__name__)
