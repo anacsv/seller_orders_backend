3f2f3f68e91c207f10ed05f5846065920362e6f8
anacsv <anapaula.carneiro@olist.com>
2020-08-20 11:29:06 -0300

    merge

--- app/start.py
+++ app/start.py
@@ -2,19 +2,6 @@ from flask import Flask, jsonify, request
 from flask_restful import Api
 from flask_cors import CORS
 
-from app.controller.selller_order_controller
-
-app = Flask(__name__)
-api = Api(app)
-cord = CORS(app)
-
-@app.route('/')
-def initial():
-    return ''
-from flask import Flask, jsonify, request
-from flask_restful import Api
-from flask_cors import CORS
-
 from app.controller.selller_order_controller import SellerOrderController
 
 app = Flask(__name__)
@@ -27,25 +14,6 @@ def initial():
 
 api.add_resource(
     SellerOrderController, '/api/sellers_order/', '/api/sellers_order/<id>', endpoint = 'sellers_order'
-)
-
-app.run(debug=True)
-from flask import Flask, jsonify, request
-from flask_restful import Api
-from flask_cors import CORS
-
-from app.controller.selller_order_controller
-
-app = Flask(__name__)
-api = Api(app)
-cord = CORS(app)
-
-@app.route('/')
-def initial():
-    return ''
-
-api.add_resource(
-    '/api/sellers_order/', '/api/sellers_order/<int:id>', endpoint = 'sellers_order'
 )
 
 app.run(debug=True)
